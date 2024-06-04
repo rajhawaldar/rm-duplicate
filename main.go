4dde3a86d35d5bb42a58f885ff7412af39d9e590
rajhawaldar <rajhawaldar.in@gmail.com>
2024-06-04 23:24:56 +0530

    Planning to make it TUI, re-writting code

--- main.go
+++ main.go
@@ -7,21 +7,39 @@ import (
 	"io"
 	"io/fs"
 	"log"
-	"os"
 	"path/filepath"
 	"strings"
+
+	"github.com/spf13/afero"
 )
 
 var (
 	dirPath     = flag.String("p", "", "Path to look for duplicate files")
+	dryRun      = flag.Bool("d", false, "Dry Run(Print the duplicate files, DO NOT DELETE)")
 	deleteCount = 0
 )
 
-type DuplicateEntry map[string][]string
+type File struct {
+	path   string
+	remove bool
+}
 
+type Record map[string][]File
+
+var exclude []string
+
+func checkExcludedPathExist(path string) bool {
+	for _, word := range exclude {
+		if strings.Contains(path, word) {
+			return true
+		}
+	}
+	return false
+}
 func main() {
 	flag.Parse()
-	dList := make(DuplicateEntry)
+	fileRecords := make(Record)
+	exclude = append(exclude, ".git", ".vscode")
 	if len(*dirPath) == 0 {
 		fmt.Println("Please use --help for how to use rm-duplicate.")
 		return
@@ -29,37 +47,35 @@ func main() {
 
 	err := filepath.Walk(*dirPath, func(path string, info fs.FileInfo, err error) error {
 		if err != nil {
-			log.Panic("failed to read directory.")
+			log.Panic(err.Error())
 			return err
 		}
-		if !info.IsDir() {
-			file, err := os.Open(path)
+		if !info.IsDir() && !checkExcludedPathExist(path) {
+			var fs = afero.NewOsFs()
+			file, err := fs.Open(path)
 			if err != nil {
 				log.Panic("failed to open a file", path)
 			}
-			defer file.Close()
 			hash := sha256.New()
 
 			if _, err := io.Copy(hash, file); err != nil {
 				log.Panic("calculating file hash failed")
 			}
+			file.Close()
 			hashSum := string(hash.Sum(nil))
-			dList[hashSum] = append(dList[hashSum], file.Name())
+			fileRecords[hashSum] = append(fileRecords[hashSum], File{
+				path:   path,
+				remove: false,
+			})
 		}
 		return nil
 	})
 	if err != nil {
 		log.Println(err)
 	}
-	for _, list := range dList {
-		if len(list) > 1 {
-			for _, file := range list[1:] {
-				if !strings.Contains(file, ".git") {
-					os.Remove(file)
-					fmt.Println(file, "deleted!")
-					deleteCount++
-				}
-			}
+	for key, records := range fileRecords {
+		if len(records) == 1 {
+			delete(fileRecords, key)
 		}
 	}
 	fmt.Println("Total", deleteCount, "files deleted!")
